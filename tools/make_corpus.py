f681fa279ae65d200bf0848916ae319e0664ce35
NeilSinclair <neil.ian.sinclair@gmail.com>
2021-09-06 20:10:52 +0200

    Some more updates for DRG

--- tools/make_corpus.py
+++ tools/make_corpus.py
@@ -10,6 +10,11 @@ parser.add_argument('--dataset',
                     type=str,
                     help="String indicating which dataset to use; must be of the format dataset_percentNoise e.g. yelp_20")
 
+parser.add_argument('--data_dir',
+                    default='./data/',
+                    type=str,
+                    help="The directory where the data sits")
+
 args = parser.parse_args()
 
 # Concatenate the negative and positive training datasets and then write to file
@@ -20,8 +25,8 @@ datasets = ['test', 'train']
 
 for set_ in datasets:
     # assume this run from the root directory
-    x0 = pd.read_csv(f'./data/{d[0]}/{d[0]}_{set_}_{d[1]}.0')
-    x1 = pd.read_csv(f'./data/{d[0]}/{d[0]}_{set_}_{d[1]}.1')
+    x0 = pd.read_csv(f'{args.data_dir}{args.dataset}/{d[0]}_{set_}_{d[1]}.0')
+    x1 = pd.read_csv(f'{args.data_dir}{args.dataset}/{d[0]}_{set_}_{d[1]}.1')
     for col in x0.columns:
         # Format the text
         x0[col] = x0[col].apply(lambda x: re.sub(r'\"', '', str(x)))
@@ -45,15 +50,15 @@ for set_ in datasets:
         # The yelp dataset is soo large, so if we're processing that, create a truncated version as this speeds
         # up the DRG evaluation a lot
         if d[0] == 'yelp' and set_=='test':
-            x0.sample(frac=0.02).to_csv(f'./data/{d[0]}/{d[0]}_{set_}_short_{d[1]}.0', index=False)
-            x1.sample(frac=0.02).to_csv(f'./data/{d[0]}/{d[0]}_{set_}_short_{d[1]}.1', index=False)
+            x0.sample(frac=0.02).to_csv(f'{args.data_dir}{args.dataset}/{d[0]}_{set_}_short_{d[1]}.0', index=False)
+            x1.sample(frac=0.02).to_csv(f'{args.data_dir}{args.dataset}/{d[0]}_{set_}_short_{d[1]}.1', index=False)
 
-        x0.to_csv(f'./data/{d[0]}/{d[0]}_{set_}_{d[1]}.0', index=False)
-        x1.to_csv(f'./data/{d[0]}/{d[0]}_{set_}_{d[1]}.1', index=False)
+        x0.to_csv(f'{args.data_dir}{args.dataset}/{d[0]}_{set_}_{d[1]}.0', index=False)
+        x1.to_csv(f'{args.data_dir}{args.dataset}/{d[0]}_{set_}_{d[1]}.1', index=False)
 
 x0 = x0.Target
 x1 = x1.Target
 
 new_df = pd.concat([x0,x1])
 
-new_df.to_csv(f'./data/{d[0]}/{d[0]}_train_{d[1]}.corpus', index=False)
+new_df.to_csv(f'{args.data_dir}{args.dataset}/{d[0]}_train_{d[1]}.corpus', index=False)
